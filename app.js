ca6a7ab78a8dcc04324f57171cd942d765d2b6de
Shriram Ramachandran <rshriramrsr2003@gmail.com>
2022-10-01 16:52:35 +0530

    Added Hashing using md5 module

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,9 +19,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model('Users', userSchema);
 
@@ -45,7 +42,7 @@ app.post('/login', function(req, res) {
             console.log(err);
         } else {
             if(user) {
-                if(user.password === password) {
+                if(user.password === md5(password)) {
                     res.render("secrets");
                 } else {
                     res.send("Wrong Password")
@@ -60,7 +57,7 @@ app.post('/login', function(req, res) {
 app.post('/register', function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if(err) {
